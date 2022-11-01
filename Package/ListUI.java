edfe6f034ce389028a23fc3d173bb5b65f634a35
Carla <carla_diana@yahoo.com>
2022-11-01 22:35:29 +0200

    am mai adaugat polimorfismul prin suprascrierea metodei getUserName din clasa "RegisterUI" in clasa "ListUI"

--- Package/ListUI.java
+++ Package/ListUI.java
@@ -2,14 +2,20 @@ package Package;
 
 import java.util.ArrayList;
 
-public class ListUI {
+public class ListUI extends RegisterUI{
 
     //vectorul de obiecte
     public static ArrayList<RegisterUI> UserList = new ArrayList<RegisterUI>();
+    private String UserName;
     //constructor
     public ListUI(){
 
     }
+    @Override //polimorfism, suprascrierea metodei getUserName
+    public String getUserName() {
+        return UserName;
+    }
+
     public void setUserList(RegisterUI user){
 
         UserList.add(user);
